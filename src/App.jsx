b3b751da789320c445591b1e77d1df763768cd2c
esprusso <vezril@gmail.com>
2026-02-17 08:24:21 -0600

    Hero Hidden

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import BlogPost from './components/BlogPost';
 
 const Home = () => (
     <>
-        <Hero />
+        {/* <Hero /> */}
         <Gallery />
         <Writing />
         <About />
