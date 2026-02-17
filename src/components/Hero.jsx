00da0da10f032a610d2e0bdbaba10a90f98e0011
esprusso <vezril@gmail.com>
2026-02-17 08:26:23 -0600

    Restored hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -156,7 +156,7 @@ const Hero = () => {
                     text-align: center;
                     padding: 2rem;
                     position: relative;
-                    background-image: linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.85)), url(/images/dd-3.jpg);
+                    /* background-image: linear-gradient(to bottom, rgba(0,0,0,0.5), rgba(0,0,0,0.85)), url(/images/dd-3.jpg); */
                     background-size: cover;
                     background-position: center;
                     color: #FFFFFF;
