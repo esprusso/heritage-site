96e91abb9f68a956f33daa76c0489ab1fd4c60ab
esprusso <vezril@gmail.com>
2026-03-21 17:30:15 -0500

    Updates

--- assets/chevron-right-D3IVKkkf.js
+++ assets/chevron-right-D3IVKkkf.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-CH4O3sYJ.js";/**
+import{c as e}from"./index-C7vxkVWk.js";/**
  * @license lucide-react v0.331.0 - ISC
  *
  * This source code is licensed under the ISC license.
