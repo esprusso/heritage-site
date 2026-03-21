8f25edf8bd53d62b82c11a74b18783e9d556ecf3
esprusso <vezril@gmail.com>
2026-03-21 15:04:20 -0500

    Updates

--- assets/chevron-right-iPjs5G1k.js
+++ assets/chevron-right-iPjs5G1k.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-D9WWos_t.js";/**
+import{c as e}from"./index-iprJMwiK.js";/**
  * @license lucide-react v0.331.0 - ISC
  *
  * This source code is licensed under the ISC license.
