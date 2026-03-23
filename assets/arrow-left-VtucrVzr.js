c4e40022894069da1774ce64ca74b5fbe4eb74f8
esprusso <vezril@gmail.com>
2026-03-23 16:57:06 -0500

    Updates

--- assets/arrow-left-VtucrVzr.js
+++ assets/arrow-left-VtucrVzr.js
@@ -1,4 +1,4 @@
-import{c as e}from"./index-D2gWlxWe.js";/**
+import{c as e}from"./index-Ci8eTUnx.js";/**
  * @license lucide-react v0.331.0 - ISC
  *
  * This source code is licensed under the ISC license.
